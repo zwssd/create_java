a2a53cc049188afb371e5bff74c6e134808b2c2f
zwssd <zwssd@9cee07ea-ca8a-48a1-8c65-f241d9734ebb>
2016-01-13 07:02:39 +0000

    已经生成了jpa文件，service层和serviceimpl层--张伟  2016.1.13

--- create_java.py
+++ create_java.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import unicodedata
 import MySQLdb as mysql
 
 import config
@@ -11,23 +12,27 @@ import db_config
 
 from string import Template
 
+
+#下划线命名格式驼峰命名格式
+def underline_to_camel(underline_format):
+
+    camel_format = ''
+    if type(underline_format)==unicode:
+        underline_format = unicodedata.normalize('NFKD',underline_format).encode('utf-8','ignore')
+    if isinstance(underline_format, str):
+        for _s_ in underline_format.split('_'):
+            camel_format += _s_.capitalize()
+    return camel_format
+
 #根据数据库的表自动生成jpa文件
-def jpaModel(parameters):
+def jpaProduceModel(parameters,results):
     if not os.path.exists(parameters[2]):
         os.mkdir(parameters[2])
-    results = connectDb()
     jpamodelClass(parameters,results)
 
-def produceModel(parameters):
-    print(parameters[2])
-    if not os.path.exists(parameters[2]):
-        os.mkdir(parameters[2])
-    modelClass(parameters)
-    defModelHBM(parameters)
-
-def produceService(parameters):
-    serviceInter(parameters)
-    serviceImpl(parameters)
+def produceService(parameters,results):
+    serviceInter(parameters,results)
+    serviceImpl(parameters,results)
 
 def produceDao(parameters):
     daoInter(parameters)
@@ -50,112 +55,162 @@ def dbtype(db_column_type):
     return val
 
 def jpamodelClass(parameters,results):
-    code = Template('''''package\n
+    code = Template('''package\n
 @Entity
 @Table(name="${tableName}")
 public class ${className}  implements Serializable{\n
 ${attribute}
 ${methods}
-}
-''')
+}''')
     name = parameters[0]    # class name
     argv2 = parameters[1]
+    path = parameters[2]
     attribute = ""
     methods = ""
 
     for x in results:
         propName = x[0]
         propType = dbtype(x[1])
-        attribute += "\t@Column(name='" + propName + "');\n\tprivate " + propType + " " + propName +";\n\n"
-        methods += "\tpublic " + propType + " get" + propName + "() {\n\treturn " + propName + ";\n\t}\n\n"
-        methods += "\tpublic void set" + propName + "(" + propType + " " + propName + ") {\n\tthis." + propName + " = " + propName + ";\n\t}\n\n"
-
-    '''properties = argv2.split(",")
-    for x in range(len(properties)):
-        prop = properties[x].split(":")
-        propType = prop[1]
-        propName = prop[0]
-        attribute += "\tprivate " + propType + " " + propName + ";\n"
-        methods += "\tpublic "+propType+" set"+propName.capitalize()+"("+propType+" "+propName+") {\n\t\tthis."+propName+" = " + propName + ";\n\t}\n"
-        methods += "\tpublic void get"+propName.capitalize()+"() {\n\t\treturn "+propName+";\n\t}\n"'''
+        propNameCamel = underline_to_camel(x[0])
+        attribute += "\t@Column(name='" + propName + "');\n\tprivate " + propType + " " + propNameCamel +";\n\n"
+        methods += "\tpublic " + propType + " get" + propNameCamel + "() {\n\t\treturn " + propNameCamel + ";\n\t}\n\n"
+        methods += "\tpublic void set" + propNameCamel + "(" + propType + " " + propNameCamel + ") {\n\t\tthis." + \
+                   propNameCamel + " = " + propNameCamel + ";\n\t}\n\n"
 
     fileStr = code.substitute(tableName=db_config.Table, className=name, attribute=attribute, methods=methods)
-    saveFile(fileStr, parameters[2] + '/' + name+".java")
+    saveFile(fileStr, path + '/' + name+".java")
 
-def modelClass(parameters):
-    code = Template('''''package\n
-public class ${className} {\n
-${attribute}
-${methods}
-}
-''')
-    name = parameters[0]    # class name
-    argv2 = parameters[1]
-    attribute = ""
-    methods = ""
-    properties = argv2.split(",")
-    for x in range(len(properties)):
-        prop = properties[x].split(":")
-        propType = prop[1]
-        propName = prop[0]
-        attribute += "\tprivate " + propType + " " + propName + ";\n"
-        methods += "\tpublic "+propType+" set"+propName.capitalize()+"("+propType+" "+propName+") {\n\t\tthis."+propName+" = " + propName + ";\n\t}\n"
-        methods += "\tpublic void get"+propName.capitalize()+"() {\n\t\treturn "+propName+";\n\t}\n"
+def serviceInter(parameters,results):
+    code = Template('''package
+public interface ${name}Service {
+    /**
+	 * 创建
+${param_notes}
+	 * @return ${name}信息
+	 */
+    public ${name} create${name}(${param});
 
-    fileStr = code.substitute(className=name, attribute=attribute, methods=methods)
-    saveFile(fileStr, parameters[2] + '/' + name+".java")
+    /**
+	 * 查询全部
+	 * @return 全部${name}列表
+	 */
+	public List<${name}> findAll${name}s();
 
-def defModelHBM(parameters):
-    code = Template('''''
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE hibernate-mapping PUBLIC
-    "-//Hibernate/Hibernate Mapping DTD 3.0//EN"
-    "http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd">
+	/**
+	 * 根据${column_first_name}查询${name}信息
+	 * @param ${column_first_type} ${column_first_name}
+	 * @return ${name}信息
+	 */
+	public ${name} findUserByName(${column_first_type} ${column_first_name});
 
-<hibernate-mapping>
-    <class name="${name}" table="${name}">
-${property}
-    </class>
-</hibernate-mapping>
-''')
-    name = parameters[0]    # class name
-    argv2 = parameters[1]
-    property = ""
-    properties = argv2.split(",")
-    for x in range(len(properties)):
-        prop = properties[x].split(":")
-        property += "\t\t<property name=\""+prop[0]+"\" />\n"
+	/**
+	 * 删除一个${name}
+	 * @param ${column_first_type} ${column_first_name}
+	 */
+	public void remove${name}(${column_first_type} ${column_first_name});
 
-    fileStr = code.substitute(name=name, property=property)
-    saveFile(fileStr, parameters[2] + '/' + name+".hbm.xml")
-
-def serviceInter(parameters):
-    code = Template('''''package\n
-public interface ${name}Service extends GenericManager<${name}, Integer> {
-
+	/**
+	 * 修改一个${name}
+	 * @param ${column_first_type} ${column_first_name}
+	 */
+	public void update${name}(${column_first_type} ${column_first_name});
 }
 ''')
     name = parameters[0]
-    fileStr = code.substitute(name=name)
-    saveFile(fileStr, parameters[2] + '/' + name+"Service.java")
+    path = parameters[2]
+    param_notes = ""
+    param = ""
+    column_first_name = ""
+    column_first_type = ""
+    column_first_bool = True
+    for x in results:
+        propName = x[0]
+        propType = dbtype(x[1])
+        propNameCamel = underline_to_camel(x[0])
+        if column_first_bool:
+            column_first_name = propNameCamel.encode("utf-8")
+            column_first_type = propType
+            column_first_bool = False
+        param_notes += "\t * @param " + propType + " " + propNameCamel + "\n"
+        if param!="":
+            propType = "," + propType
+        param += propType + " " + propNameCamel
+    fileStr = code.substitute(name=name,param_notes=param_notes,param=param,column_first_name=column_first_name,column_first_type=column_first_type)
+    saveFile(fileStr, path + '/' + name+"Service.java")
 
-def serviceImpl(parameters):
-    code = Template('''''package\n
-public class ${name}ServiceImpl extends GenericManagerImpl<${name}, Integer>
-        implements ${name}Service{
+def serviceImpl(parameters,results):
+    code = Template('''package\n
+@Service
+public class ${name}ServiceImpl implements ${name}Service{
 
+    @Resource
     private ${name}Dao ${namelow}Dao;
 
-    public ${name}ServiceImpl(${name}Dao ${namelow}Dao){
-        super(${namelow}Dao);
-        this.${namelow}Dao = ${namelow}Dao;
+    /**
+	 * 创建
+${param_notes}
+	 * @return ${name}信息
+	 */
+    public ${name} create${name}(${param}){
+
     }
+
+    /**
+	 * 查询全部
+	 * @return 全部${name}列表
+	 */
+	public List<${name}> findAll${name}s(){
+
+	}
+
+	/**
+	 * 根据${column_first_name}查询${name}信息
+	 * @param ${column_first_type} ${column_first_name}
+	 * @return ${column_first_type} ${column_first_name}信息
+	 */
+	public ${name} findUserByName(${column_first_type} ${column_first_name}){
+
+	}
+
+	/**
+	 * 删除一个${name}
+	 * @param ${column_first_type} ${column_first_name}
+	 */
+	public void remove${name}(${column_first_type} ${column_first_name}){
+
+	}
+
+	/**
+	 * 修改一个${name}
+	 * @param ${column_first_type} ${column_first_name}
+	 */
+	public void update${name}(${column_first_type} ${column_first_name}){
+
+	}
 }
 ''')
     name = parameters[0]
     namelow = name.lower()
-    fileStr = code.substitute(name=name, namelow=namelow)
-    saveFile(fileStr, parameters[2] + '/' + name+"ServiceImpl.java")
+    path = parameters[2]
+    param_notes = ""
+    param = ""
+    column_first_name = ""
+    column_first_type = ""
+    column_first_bool = True
+    for x in results:
+        propName = x[0]
+        propType = dbtype(x[1])
+        propNameCamel = underline_to_camel(x[0])
+        if column_first_bool:
+            column_first_name = propNameCamel.encode("utf-8")
+            column_first_type = propType
+            column_first_bool = False
+        param_notes += "\t * @param " + propType + " " + propNameCamel + "\n"
+        if param!="":
+            propType = "," + propType
+        param += propType + " " + propNameCamel
+    fileStr = code.substitute(name=name,namelow=namelow,param_notes=param_notes,param=param,column_first_name=column_first_name,column_first_type=column_first_type)
+    saveFile(fileStr, path + '/' + name+"ServiceImpl.java")
 
 def daoInter(parameters):
     code = Template('''''package\n
@@ -200,9 +255,9 @@ def main():
     if (number < 2):
         print "Error parameters"
     else :
-        jpaModel(parameters)
-        #produceModel(parameters)
-        #produceService(parameters)
+        results = connectDb()
+        jpaProduceModel(parameters,results)
+        produceService(parameters,results)
         #produceDao(parameters)
 
 if __name__ == "__main__":
